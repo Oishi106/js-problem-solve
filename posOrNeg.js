f71cb33d81ee221cab927b27d18aa11fd94b4008
Oishi106l <mahmudaoishi457@gmail.com>
2026-06-08 17:17:58 +0600

    Problem 5: Check Positive, Negative or Zero

---
+++ posOrNeg.js
@@ -0,0 +1,23 @@
+/*
+Problem 5: Check Positive, Negative or Zero  [Easy]
+Description: Write a function checkSign(n) that returns 'positive', 'negative', or 'zero' based on the value of n.
+Example:
+Input: -5  → Output: 'negative'Input: 0   → Output: 'zero'
+Hint: Use if-else if-else statements.
+
+*/
+
+function checkSign(n) {
+    if (n > 0) {
+        return "positive";
+    } else if (n < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+
+// Examples
+console.log(checkSign(-5)); // negative
+console.log(checkSign(0));  // zero
+console.log(checkSign(10)); // positive
