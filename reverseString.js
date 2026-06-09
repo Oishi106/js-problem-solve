c6cc47dbe64ade1eabe427691d35aaad37b1401c
Oishi106l <mahmudaoishi457@gmail.com>
2026-06-09 17:56:37 +0600

    Problem 6: Reverse a String

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example Usage
+console.log(reverseString("hello")); // olleh
+console.log(reverseString("world")); // dlrow
