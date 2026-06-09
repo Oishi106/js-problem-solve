28e51ba4153bf68583c3ef94f3e55c87bb6f843f
Oishi106l <mahmudaoishi457@gmail.com>
2026-06-09 18:04:57 +0600

    Problem 10: Count Occurrences of a Character

---
+++ countOccurrences.js
@@ -0,0 +1,17 @@
+/*
+Problem 10: Count Occurrences of a Character  [Easy]
+Description: Write a function countChar(str, char) that returns how many times a character appears in a string.
+Example:
+Input: 'banana', 'a'  → Output: 3
+Hint: Use split(char).length - 1 or a loop.
+
+*/
+
+
+function countChar(str, char) {
+    return str.split(char).length - 1;
+}
+
+// Example Usage
+console.log(countChar("banana", "a")); // 3
+console.log(countChar("hello", "l"));  // 2
