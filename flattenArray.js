9a30f57412293fc17ca3ab6474235b4c14c19cca
Oishi106l <mahmudaoishi457@gmail.com>
2026-06-10 18:59:05 +0600

    Problem 14: Flatten a Nested Array

---
+++ flattenArray.js
@@ -0,0 +1,18 @@
+/*
+
+Problem 14: Flatten a Nested Array  [Medium]
+Description: Write a function flattenArray(arr) that flattens one level of a nested array.
+Example:
+Input: [1, [2, 3], [4, 5]]  → Output: [1, 2, 3, 4, 5]
+Hint: Use flat() or reduce() with concat().
+
+*/
+
+
+function flattenArray(arr) {
+    return arr.reduce((acc, item) => acc.concat(item), []);
+}
+
+// Example
+console.log(flattenArray([1, [2, 3], [4, 5]])); 
+// Output: [1, 2, 3, 4, 5]
