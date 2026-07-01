1202adc6c200caafde6b48ec94d4a8eb7c24dcd1
Oishi106l <mahmudaoishi457@gmail.com>
2026-07-01 23:27:17 +0600

    updated

--- sumOfArray.js
+++ sumOfArray.js
@@ -7,15 +7,15 @@ Hint: Use reduce() or a for loop.
 
 */
 
-function sumArray(arr) {                                                       
+function sumArray(arr) {                                                                                       
     let sum = 0;
 
     for (let i = 0; i < arr.length; i++) {                                        
         sum += arr[i];                                                                        
-    }
+    }                  
 
-    return sum;                   
+    return sum;                                              
 }
 
 // Example
-console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15               
